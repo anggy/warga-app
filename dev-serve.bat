b60d650627f75961d422e75f6c58020591b89cc9
anggy maulana ariefin <anggy.all@gmail.com>
2026-01-09 02:19:57 +0700

    update

--- dev-serve.bat
+++ dev-serve.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting Laravel Server with SQLite support...
-echo Open http://127.0.0.1:8000/warga/login in your browser.
-call php -d extension=intl -d extension=pdo_mysql artisan optimize:clear
+echo Open http://127.0.0.1:8001/warga/login in your browser.
+call php artisan optimize:clear
 cd public
-php -d extension=intl -d extension=pdo_mysql -S 127.0.0.1:8000 ../server.php
+php -S 127.0.0.1:8001 ../server.php
 pause
--- dev-serve.sh
+++ dev-serve.sh
@@ -4,9 +4,9 @@ echo "Starting Laravel Server with SQLite support..."
 echo "Open http://127.0.0.1:8001/warga/login in your browser."
 
 # Clear cache with necessary extensions enabled
-php -d extension=intl -d extension=pdo_mysql artisan optimize:clear
+php artisan optimize:clear
 
 cd public
 
 # Start PHP built-in server
-php -d extension=intl -d extension=pdo_mysql -S 0.0.0.0:8001 ../server.php
+php -S 0.0.0.0:8001 ../server.php
